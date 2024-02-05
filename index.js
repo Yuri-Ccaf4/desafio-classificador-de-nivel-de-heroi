72b822222ef72cca7741a5ba53d366a96f9efe9e
Yuri-Ccaf4 <yurypunk@gmail.com>
2024-02-05 20:16:24 -0300

    Desafio finalizado.

---
+++ index.js
@@ -0,0 +1,24 @@
+const nomeDoHeroi = "Café";
+let xp = 5000;
+
+let nivelHeroi;
+
+if (xp <= 1000) {
+    nivelHeroi = "Ferro";
+}else if (xp > 1001 && xp <= 2000) {
+    nivelHeroi - "Bronze";
+}else if (xp > 2001 && xp <= 5000) {
+    nivelHeroi = "Prata";
+}else if (xp > 5001 && xp <= 7000) {
+    nivelHeroi = "Ouro";
+}else if (xp > 7001 && xp <= 8000) {
+    nivelHeroi = "Platina";
+}else if (xp > 8001 && xp <= 9000) {
+    nivelHeroi = "Ascendente";
+}else if (xp > 9001 && xp <= 10001) {
+    nivelHeroi = "Imortal";
+}else {
+    nivelHeroi = "Radiente";
+};
+
+console.log("O herói de nome: " + nomeDoHeroi + " está no nível " + nivelHeroi);
